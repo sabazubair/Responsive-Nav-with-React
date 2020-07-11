b0de893d34fce8be4b50c364ec01b976a2f8b60f
Saba Zubair <saba@postageapp.com>
2020-07-11 15:16:54 -0400

    responsive navigation bar & side drawer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Toolbar from "./components/Toolbar/Toolbar";
+import SideDrawer from "./components/SideDrawer/SideDrawer";
+import BackDrop from "./components/BackDrop/BackDrop";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    sideDrawerOpen: false,
+  };
+
+  // fat arrow notation adds method as a property onto the App object + ensures `this` keeps scope, refers to App component
+  drawerToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return { sideDrawerOpen: !prevState.sideDrawerOpen };
+    });
+  };
+
+  backdropClickHandler = () => {
+    this.setState({ sideDrawerOpen: false });
+  };
+  render() {
+    let backdrop;
+    if (this.state.sideDrawerOpen) {
+      backdrop = <BackDrop click={this.backdropClickHandler} />;
+    }
+    return (
+      <div className="App" style={{ height: "100%" }}>
+        <Toolbar drawerClickHandler={this.drawerToggleClickHandler} />
+        <SideDrawer show={this.state.sideDrawerOpen} />
+        {backdrop}
+        <main style={{ marginTop: "64px" }}>
+          <p>This is the page content.</p>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/BackDrop/BackDrop.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./BackDrop.css";
+
+const BackDrop = (props) => <div className="BackDrop" onClick={props.click} />;
+
+export default BackDrop;
